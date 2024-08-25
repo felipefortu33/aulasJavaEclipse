0cfdc01e925cc9d3b73453072d28efe76992fba5
felipe <felipefortu33@gmail.com>
2024-08-25 16:53:25 -0300

    Aluno - Criação de Classe

--- src/aula01/Aluno.java
+++ src/aula01/Aluno.java
@@ -1,17 +1,41 @@
 package aula01;
 
-public class Aluno {
-	private String nome;
-	public void inserirNome(String nn ) {
-		nome = nn;
-	}
-	public String recuperarNome() {
-		return nome;
-	}
-	public static void main(String args[]) {
-		Aluno a = new Aluno();
-		a.inserirNome("Felipe");
-		System.out.println("saida: "+a.recuperarNome());
-	}
+import java.util.Random;
 
+public class Aluno {
+    // Atributos
+    private String nome;
+    private int idade;
+    private long codigo_identificador;
+    private Random aleatorio;
+    
+    // Construtor
+    public Aluno(String nome, int idade) {
+        aleatorio = new Random();
+        this.nome = nome;
+        this.idade = idade;
+        this.codigo_identificador = aleatorio.nextLong(); // Gera um identificador long
+    }
+    
+    // Métodos
+    public void definirNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void definirIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    // Método main para testar a classe
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("João", 20);
+        System.out.println("Nome: " + aluno.nome);
+        System.out.println("Idade: " + aluno.idade);
+        System.out.println("Código Identificador: " + aluno.codigo_identificador);
+    }
 }
+
+
+
+
+
