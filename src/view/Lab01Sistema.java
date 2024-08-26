d00bc123d90b267cccb224eae2a71c67f571b269
felipe <felipefortu33@gmail.com>
2024-08-26 17:48:12 -0300

    Lab02Sistema

--- src/view/Lab01Sistema.java
+++ src/view/Lab01Sistema.java
@@ -3,40 +3,60 @@ package view;
 import java.util.Scanner;
 
 public class Lab01Sistema {
-	public static void main(String[] args) {
-		int opcao = 0;
+    // Criar uma única instância do Scanner para reutilização
+    private static Scanner leia = new Scanner(System.in);
 
-		while (opcao != 9) {
+    public static void main(String[] args) {
+        int opcao = 0;
 
-			Scanner leia = new Scanner(System.in);
+        while (opcao != 9) {
+            System.out.println("1 - Cadastramento");
+            System.out.println("2 - Saque");
+            System.out.println("3 - Deposito");
+            System.out.println("9 - Fim");
+            System.out.println("Digite sua opção: ");
+            opcao = leia.nextInt();
 
-			System.out.println("1 - Cadastramento");
-			System.out.println("2 - Saque");
-			System.out.println("3 - Deposito");
-			System.out.println("9 - Fim");
-			System.out.println("Digite sua opção: ");
-			opcao = leia.nextInt();
-			if (opcao == 1) {
-				execCadastramento();
-			}
-		}
-	}
-	public static void execCadastramento() {
-		Scanner leia = new Scanner (System.in);
-		System.out.println("Numero da Agencia: ");
-		int agencia = leia.nextInt();
-		System.out.println("Numero da Conta: ");
-		int conta = leia.nextInt();
-		System.out.println("Nome do Cliente: ");
-		String nome = leia.next();
-		System.out.println("Saldo: ");
-		double saldo = leia.nextDouble();
-		System.out.println("Confirma cadastramento: ");
-		String cad = leia.next();
-		if (cad.equalsIgnoreCase("s")) {
-			System.out.println("Cadastro Realizado com sucesso");
-			
-		}
-		
-	}
+            if (opcao == 1) {
+                execCadastramento();
+            } else if (opcao == 2) {
+                execSaque();
+            } else if (opcao != 9) {
+                System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
+
+        // Fechar o Scanner ao final do uso
+        leia.close();
+    }
+
+    public static void execCadastramento() {
+        System.out.println("Numero da Agencia: ");
+        int agencia = leia.nextInt();
+        System.out.println("Numero da Conta: ");
+        int conta = leia.nextInt();
+        System.out.println("Nome do Cliente: ");
+        String nome = leia.next();
+        System.out.println("Saldo: ");
+        double saldo = leia.nextDouble();
+        System.out.println("Confirma cadastramento (S/N): ");
+        String cad = leia.next();
+        if (cad.equalsIgnoreCase("s")) {
+            System.out.println("Cadastro Realizado com sucesso");
+        }
+    }
+
+    public static void execSaque() {
+        System.out.println("Numero da Agencia :");
+        int agencia = leia.nextInt();
+        System.out.println("Numero da Conta   :");
+        int conta = leia.nextInt();
+        System.out.println("Valor do Saque: ");
+        double saque = leia.nextDouble();
+        System.out.println("Confirma saque (S/N) :");
+        String cad = leia.next();
+        if (cad.equalsIgnoreCase("s")) {
+            System.out.println("Saque realizado");
+        }
+    }
 }
