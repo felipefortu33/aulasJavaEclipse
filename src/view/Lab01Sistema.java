572df481076519e6f382b6dc113b35e1561a49bf
felipe <felipefortu33@gmail.com>
2024-08-26 19:44:36 -0300

    La01Sistema, completo com switch

--- src/view/Lab01Sistema.java
+++ src/view/Lab01Sistema.java
@@ -3,7 +3,8 @@ package view;
 import java.util.Scanner;
 
 public class Lab01Sistema {
-    // Criar uma única instância do Scanner para reutilização
+
+    // Instanciar o Scanner uma vez
     private static Scanner leia = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -14,49 +15,77 @@ public class Lab01Sistema {
             System.out.println("2 - Saque");
             System.out.println("3 - Deposito");
             System.out.println("9 - Fim");
-            System.out.println("Digite sua opção: ");
+            System.out.print("Digite sua opção: ");
             opcao = leia.nextInt();
 
-            if (opcao == 1) {
-                execCadastramento();
-            } else if (opcao == 2) {
-                execSaque();
-            } else if (opcao != 9) {
-                System.out.println("Opção inválida! Tente novamente.");
+            switch (opcao) {
+                case 1:
+                    execCadastramento();
+                    break;
+                case 2:
+                    execSaque();
+                    break;
+                case 3:
+                    execDeposito();
+                    break;
+                case 9:
+                    System.out.println("Fim do programa.");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
             }
         }
-
-        // Fechar o Scanner ao final do uso
-        leia.close();
+        leia.close(); // Fechar o Scanner no final
     }
 
     public static void execCadastramento() {
-        System.out.println("Numero da Agencia: ");
+        System.out.print("Número da Agência: ");
         int agencia = leia.nextInt();
-        System.out.println("Numero da Conta: ");
+        System.out.print("Número da Conta: ");
         int conta = leia.nextInt();
-        System.out.println("Nome do Cliente: ");
+        System.out.print("Nome do Cliente: ");
         String nome = leia.next();
-        System.out.println("Saldo: ");
+        System.out.print("Saldo: ");
         double saldo = leia.nextDouble();
-        System.out.println("Confirma cadastramento (S/N): ");
+        System.out.print("Confirma cadastramento (S/N): ");
         String cad = leia.next();
         if (cad.equalsIgnoreCase("s")) {
-            System.out.println("Cadastro Realizado com sucesso");
+            System.out.println("Cadastro realizado com sucesso.");
+        } else {
+            System.out.println("Cadastro não realizado.");
         }
     }
 
     public static void execSaque() {
-        System.out.println("Numero da Agencia :");
+        System.out.print("Número da Agência: ");
         int agencia = leia.nextInt();
-        System.out.println("Numero da Conta   :");
+        System.out.print("Número da Conta: ");
         int conta = leia.nextInt();
-        System.out.println("Valor do Saque: ");
+        System.out.print("Valor do Saque: ");
         double saque = leia.nextDouble();
-        System.out.println("Confirma saque (S/N) :");
-        String cad = leia.next();
-        if (cad.equalsIgnoreCase("s")) {
-            System.out.println("Saque realizado");
+        System.out.print("Confirma saque (S/N): ");
+        String saq = leia.next();
+        if (saq.equalsIgnoreCase("s")) {
+            System.out.println("Saque realizado.");
+        } else {
+            System.out.println("Saque não realizado.");
+        }
+    }
+
+    public static void execDeposito() {
+        System.out.print("Número da Agência: ");
+        int agencia = leia.nextInt();
+        System.out.print("Número da Conta: ");
+        int conta = leia.nextInt();
+        System.out.print("Valor do Depósito: ");
+        double deposito = leia.nextDouble();
+        System.out.print("Confirma depósito (S/N): ");
+        String dep = leia.next();
+        if (dep.equalsIgnoreCase("s")) {
+            System.out.println("Depósito realizado.");
+        } else {
+            System.out.println("Depósito não realizado.");
         }
     }
 }
